7956979f5d21df0bf4a4702185c41f2ced1becad
peterfiasco <peter.okachie@gmail.com>
2025-03-29 16:25:58 +0100

    Fix render-build.sh syntax errors and add TypeScript fixes 2

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,7 +2,6 @@ declare module 'body-parser';
 declare module 'jsonwebtoken' {
   interface JwtPayload {
     id: string;
-    // Add other properties that might be in your JWT token
   }
 }
 declare module 'pdfkit';
